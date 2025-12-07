9cb9c14d0d871dbb5737b674bc371acf22fdabc5
DiajengAyuCinta <diajencinta@gmail.com>
2025-12-07 21:04:35 +0700

    Add files via upload

--- p8_pagination/pagination.php
+++ p8_pagination/pagination.php
@@ -1,1 +1,18 @@
+<?php
+function paginate($conn, $query, $per_page = 5) {
+    $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
 
+    $start = ($page - 1) * $per_page;
+
+    $result = mysqli_query($conn, $query . " LIMIT $start, $per_page");
+
+    $total_records = mysqli_num_rows(mysqli_query($conn, $query));
+    $total_pages = ceil($total_records / $per_page);
+
+    return [
+        "result" => $result,
+        "page" => $page,
+        "total_pages" => $total_pages
+    ];
+}
+?>
